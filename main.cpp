3340e2874a0e4cdcef434715bfb163cf7dddbd6e
Dmitry Sorokin <molotov.dmitry@gmail.com>
2019-01-26 21:24:08 +0300

    Initial commit.

---
+++ main.cpp
@@ -0,0 +1,112 @@
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <errno.h>
+
+struct FilesHandles
+{
+    std::istream* fileInputPtr;
+    std::ifstream fileInput;
+    std::ofstream fileServerSource;
+    std::ofstream fileServerHeader;
+    std::ofstream fileClientSource;
+    std::ofstream fileClientHeader;
+};
+
+bool openOutputFile(const char* path, std::ofstream& file)
+{
+    file.open(path);
+
+    if (not file.is_open())
+    {
+        //TODO: error
+        return false;
+    }
+
+    return true;
+}
+
+int main(int argc, char* argv[])
+{
+    FilesHandles filesHandles;
+    filesHandles.fileInputPtr = &std::cin;
+
+    //// Open files ============================================================
+
+    for (int i = 1; i < argc; i += 2)
+    {
+        if (strcmp(argv[i], "-i") == 0)
+        {
+            if (i == argc - 1)
+            {
+                //TODO: error
+            }
+
+            filesHandles.fileInput.open(argv[i + 1]);
+            if (not filesHandles.fileInput.is_open())
+            {
+                //TODO: error
+                return 1;
+            }
+
+            filesHandles.fileInputPtr = &filesHandles.fileInput;
+        }
+        else if (strcmp(argv[i], "-ss") == 0)
+        {
+            if (i == argc - 1)
+            {
+                //TODO: error
+            }
+
+            if (not openOutputFile(argv[i + 1], filesHandles.fileServerSource))
+            {
+                return 1;
+            }
+        }
+        else if (strcmp(argv[i], "-sh") == 0)
+        {
+            if (i == argc - 1)
+            {
+                //TODO: error
+            }
+
+            if (not openOutputFile(argv[i + 1], filesHandles.fileServerHeader))
+            {
+                return 1;
+            }
+        }
+        else if (strcmp(argv[i], "-cs") == 0)
+        {
+            if (i == argc - 1)
+            {
+                //TODO: error
+            }
+
+            if (not openOutputFile(argv[i + 1], filesHandles.fileClientSource))
+            {
+                return 1;
+            }
+        }
+        else if (strcmp(argv[i], "-ch") == 0)
+        {
+            if (i == argc - 1)
+            {
+                //TODO: error
+            }
+
+            if (not openOutputFile(argv[i + 1], filesHandles.fileClientHeader))
+            {
+                return 1;
+            }
+        }
+    }
+
+    //// Read input file =======================================================
+
+    for(std::string line; getline(*filesHandles.fileInputPtr, line );)
+    {
+
+    }
+}
